5b1e24fe30223593e32930224054063da595d4d2
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-09 12:58:25 -0500

    2

--- Topic2MathAssignment/Program.cs
+++ Topic2MathAssignment/Program.cs
@@ -7,15 +7,18 @@
             //My name is Mercedes, F
             Console.WriteLine("I was born in " + (2026 - 17));
             Console.WriteLine(    );
-            Console.WriteLine("Average of 14, 15, 16");
+            Console.WriteLine("The average of 14, 15, 16 is ");
+            //Add all numbers together and divide by how many numbers there are
+            Console.WriteLine((14.0 + 15.0 + 16.0 / 3));
             Console.WriteLine(    );
-            Console.WriteLine("Average of 14, 15, 16, 17");
+            Console.WriteLine("The average of 14, 15, 16, 17 is ");
+            Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0 / 4));
             Console.WriteLine(    );
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
+            //pi * R^2 = Area of a circle
+            Console.WriteLine("What's the area of a 3cm radius circle? ");
+            Console.WriteLine((3.14 * 3 + "3" + 2) + "cm^2");
+            Console.WriteLine(    );
+            Console.WriteLine("C# knows when to add using the + symbol based on the use of quotations.");
             Console.WriteLine();
         }
     }
