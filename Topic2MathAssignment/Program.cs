d058541995040f901bf55046c56b1581db9b0467
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-09 13:12:45 -0500

    3

--- Topic2MathAssignment/Program.cs
+++ Topic2MathAssignment/Program.cs
@@ -9,17 +9,18 @@
             Console.WriteLine(    );
             Console.WriteLine("The average of 14, 15, 16 is ");
             //Add all numbers together and divide by how many numbers there are
-            Console.WriteLine((14.0 + 15.0 + 16.0 / 3));
+            Console.WriteLine(14.0 + 15.0 + 16.0 / 3);
             Console.WriteLine(    );
             Console.WriteLine("The average of 14, 15, 16, 17 is ");
-            Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0 / 4));
+            Console.WriteLine(14.0 + 15.0 + 16.0 + 17.0 / 4);
             Console.WriteLine(    );
             //pi * R^2 = Area of a circle
             Console.WriteLine("What's the area of a 3cm radius circle? ");
             Console.WriteLine((3.14 * 3 + "3" + 2) + "cm^2");
-            Console.WriteLine(    );
+            //Math.PI is accurate but longer
+            Console.WriteLine(Math.PI * 3 + "3" + 2 + "cm^2");
             Console.WriteLine("C# knows when to add using the + symbol based on the use of quotations.");
-            Console.WriteLine();
+            Console.WriteLine(Math.Round);
         }
     }
 }
